f579d53b147a625cf9d44d3f2352e66c4d32e998
Aliaksandr Salata <sasvanis@gmail.com>
2022-11-13 15:23:54 +0300

    First homework compleate

--- H1/Program.cs
+++ H1/Program.cs
@@ -1,2 +1,68 @@
 ﻿// Домашняя работа
-Console.Write("2325");
+
+/*
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+int min = 0;
+
+if (num1 > num2) max = num1; min = num2;
+if (num2 > num1) max = num2; min = num1;
+
+Console.Write($"Its maximum number {max} and its minimum number {min}");
+*/
+/*
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Input first int number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second int number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third int number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.Write($"Its maximum number {max}");
+*/
+/*
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input int number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+          Console.Write($"Its number {num} is even number");
+        }
+        else
+        {
+          Console.Write($"Its number {num} is odd number");
+        }
+
+ */
+/*
+// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input int number:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int evenNum = 2;
+
+
+if (num > 1){
+    while (evenNum <= num)
+{
+    Console.Write(evenNum + " ");
+    evenNum = evenNum + 2;
+}
+}
+*/
